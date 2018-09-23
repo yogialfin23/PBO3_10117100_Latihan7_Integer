ed57d41e3f9ce2112fccc428d4776e16b0f1c31e
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:43:26 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan7_Integer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan7_integer;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan7_Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 9000000000L;
+        System.out.println("byte : " +b);
+        System.out.println("short : " +s);
+        System.out.println("int : " +i);
+        System.out.println("long : " +l);
+    }
+    
+}
